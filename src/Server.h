54cc3ac94572a1c52389420878a19285852ee5cc
Curve <fynnbwdt@gmail.com>
2020-07-13 21:42:44 +0200

    Potential Fix for #2

--- src/Server.h
+++ src/Server.h
@@ -246,9 +246,9 @@ protected:
 				lobby.maxJokerRequests = packet.maxJokerRequests;
 				lobby.jokerCardsToDeck = packet.jokerCardsToDeck;
 				lobby.winnerBecomesCzar = packet.winnerBecomesCzar;
-				
+
 				lobbies->insert({ lobby.id, lobby });
-				
+
 				con.lobbyId = lobby.id;
 				lobbies->at(lobby.id).forceConnectAndCreate(con, packet, requestId);
 
@@ -401,17 +401,24 @@ protected:
 	context_ptr on_tls_init(hdl hdl) {
 		namespace asio = websocketpp::lib::asio;
 
-		context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::tlsv1);
+		context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
 		linfo("TLS Init called!");
 
 		try
 		{
-			ctx->set_options(boost::asio::ssl::context::default_workarounds |
-				boost::asio::ssl::context::no_sslv2 |
-				boost::asio::ssl::context::no_sslv3 |
-				boost::asio::ssl::context::single_dh_use);
+			ctx->set_options(asio::ssl::context::default_workarounds |
+				asio::ssl::context::no_sslv2 |
+				asio::ssl::context::no_sslv3 |
+				asio::ssl::context::no_tlsv1 |
+				asio::ssl::context::single_dh_use);
 			ctx->use_certificate_chain_file("./cert/cert.pem");
 			ctx->use_private_key_file("./cert/priv.pem", boost::asio::ssl::context::pem);
+			ctx->use_tmp_dh_file("./cert/dh.pem");
+
+			std::string ciphers("ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK");
+			if (SSL_CTX_set_cipher_list(ctx->native_handle(), ciphers.c_str()) != 1) {
+				lerror("Error setting cipher list");
+			}
 		}
 		catch (std::exception& e) {
 			lerror("Exception: ", e.what());

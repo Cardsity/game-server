db005c52239831dca55380e31381d5623a7644fd
Curve <fynnbwdt@gmail.com>
2020-11-26 16:33:17 +0100

    feat: Update Status-Packet

--- src/packets/server/status.h
+++ src/packets/server/status.h
@@ -4,16 +4,32 @@
 
 namespace Cardsity::Packets::Responses
 {
+    enum Status : std::uint8_t
+    {
+        INVALID_REQUEST,
+        INVALID_PLAYER,
+        INVALID_GAME,
+        BAD_NAME,
+        BAD_MESSAGE,
+        BAD_PASSWORD,
+        NOT_CZAR,
+        NOT_HOST,
+        CARD_NOT_OWNED,
+        UNKNOWN,
+        ALREADY_PLAYED,
+        KICK_SUCCESS,
+        ALREADY_INGAME,
+    };
     struct GenericStatus
     {
         bool success;
-        std::string info;
+        Status status;
     };
 } // namespace Cardsity::Packets::Responses
 
 REGISTER
 {
     using namespace Cardsity::Packets::Responses;
-    class_(GenericStatus).property(&GenericStatus::success, "success").property(&GenericStatus::info, "info");
+    class_(GenericStatus).property(&GenericStatus::success, "success").property(&GenericStatus::status, "status");
 }
 FINISH
